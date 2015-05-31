8f7475d9ed211551035bf58ae549bcd0527aed00
Ryan Taylor Long <ryan@rtlong.com>
2015-05-30 20:30:09 -0700

    Remove Badge and Clubnet from index.js

--- index.js
+++ index.js
@@ -3,7 +3,5 @@
 import * as dag from './lib/dag-object'
 import * as util from './lib/util'
 import IPFSClient from './lib/ipfs-api-client'
-import Badge from './lib/badge'
-import Clubnet from './lib/clubnet'
 
-export { dag, IPFSClient, Badge, Clubnet, util }
+export { dag, IPFSClient, util }
