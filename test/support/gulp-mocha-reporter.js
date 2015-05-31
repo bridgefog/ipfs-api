310985270c1a012ffa23e57937af288715978cce
Ryan Taylor Long <ryan@rtlong.com>
2015-05-30 19:37:47 -0700

    Add a note to explain gulp-mocha-reporter

--- test/support/gulp-mocha-reporter.js
+++ test/support/gulp-mocha-reporter.js
@@ -1,5 +1,9 @@
 import Base from 'mocha/lib/reporters/base'
 
+// The purpose of this reporter is to provide a less noisy gulp report from
+// mocha, so when tests are passing you don't see the full doc-style outline of
+// all the passing tests. On failure, you'll see the full list along with errors.
+
 module.exports = function (runner) {
   Base.call(this, runner)
 
